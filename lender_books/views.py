524fcb753134cfe69614cbd19e976be27726cc67
Scott Currie <vitriolik@gmail.com>
2019-01-09 08:28:19 -0800

    Defined book_list_view and book_detail_view

--- lender_books/views.py
+++ lender_books/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
-# Create your views here.
+from .models import Book
+
+
+def book_detail_view(request, pk=None):
+    """
+    """
+    context = {
+        'book': get_object_or_404(Book, id=pk)
+    }
+    return render(request, 'books/book_detail.html', context)
+
+
+def book_list_view(request):
+    """
+    """
+    context = {
+        'books': get_list_or_404(Book)
+    }
+    return render(request, 'books/book_list.html', context)
