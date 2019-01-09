5bc824dcce8f58bd20957bc6b7404a41ab598abc
Scott Currie <vitriolik@gmail.com>
2019-01-09 08:25:55 -0800

    Added app definitions

--- django_lender/settings.py
+++ django_lender/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split()
 # Application definition
 
 INSTALLED_APPS = [
+    'django_lender',
+    'lender_books',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
