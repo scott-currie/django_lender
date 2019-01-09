451629a2d262b3c1e73517efffa741147d5422d8
Scott Currie <vitriolik@gmail.com>
2019-01-09 15:27:28 -0800

    Added docstrings

--- lender_books/views.py
+++ lender_books/views.py
@@ -4,7 +4,10 @@ from .models import Book
 
 
 def book_detail_view(request, pk=None):
-    """
+    """Handle requests on /book/<id> and display a specific book.
+
+    :input: request: incoming request
+    :return: render the book detail template
     """
     context = {
         'book': get_object_or_404(Book, id=pk)
@@ -13,7 +16,10 @@ def book_detail_view(request, pk=None):
 
 
 def book_list_view(request):
-    """
+    """Handle requests on /book and display all books.
+
+    :input: request: incoming request
+    :return: render the book detail template
     """
     context = {
         'books': get_list_or_404(Book)
