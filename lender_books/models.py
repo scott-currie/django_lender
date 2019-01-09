ad0c894c11d923b3443c57d08e613bf85fc67e36
Scott Currie <vitriolik@gmail.com>
2019-01-09 08:27:07 -0800

    Added Book model

--- lender_books/models.py
+++ lender_books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    """Represents a book in the system.
+    """
+    cover_image = models.CharField(max_length=256)
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=256)
+    year = models.IntegerField()
+    AVAILABILITIES = [
+        ('available', 'Available'),
+        ('checked-out', 'Checked-Out')
+    ]
+    status = models.CharField(
+        max_length=128, default='available', choices=AVAILABILITIES)
+    date_added = models.DateField(auto_now=False, auto_now_add=True)
+    last_borrowed = models.DateField()
+
+    def __str__(self):
+        return f'{self.title} ({ self.status })'
