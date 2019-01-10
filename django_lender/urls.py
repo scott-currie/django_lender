1e252974c7ebb66edcce11e40a66eaad58024ef1
Scott Currie <vitriolik@gmail.com>
2019-01-10 08:49:49 -0800

    Added accounts url

--- django_lender/urls.py
+++ django_lender/urls.py
@@ -20,5 +20,7 @@ from .views import home_view
 urlpatterns = [
     path('', home_view, name='home'),
     path('admin/', admin.site.urls),
-    path('books/', include('lender_books.urls'))
+    path('books/', include('lender_books.urls')),
+    # path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
