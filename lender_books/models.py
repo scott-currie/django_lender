97700061be586671cb0eb6ae7cd0c571bff92e51
Scott Currie <vitriolik@gmail.com>
2019-01-09 16:41:08 -0800

    Fixed cover image upload. last_borrowed now allows blank and null

--- lender_books/models.py
+++ lender_books/models.py
@@ -1,12 +1,13 @@
 from datetime import date
 from django.db import models
+from django.conf import settings
 
 
 class Book(models.Model):
     """Represents a book in the system.
     """
     cover_image = models.ImageField(
-        upload_to=None, height_field=None, width_field=None, max_length=None)
+        upload_to=settings.IMAGE_DIR, height_field=None, width_field=None, max_length=None)
     title = models.CharField(max_length=256)
     author = models.CharField(max_length=256)
     YEARS = [(y, y) for y in range(date.today().year, 1453, -1)]
@@ -18,7 +19,7 @@ class Book(models.Model):
     status = models.CharField(
         max_length=128, default='available', choices=AVAILABILITIES)
     date_added = models.DateField(auto_now=False, auto_now_add=True)
-    last_borrowed = models.DateField()
+    last_borrowed = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.title} ({ self.status })'
