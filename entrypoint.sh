0edd6539751acc99da07de109f30641e94739ad3
Scott Currie <vitriolik@gmail.com>
2019-01-14 10:21:03 -0800

    Updated entrypoint to run tests on container up

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,8 @@ cd /src
 
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
+
+coverage run manage.py test -v 2
+coverage report
+
 python3 manage.py runserver 0.0.0.0:8000
